e0d074bb654d90b5dac3bd152342d904ad132169
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2023-05-29 15:51:04 +0530

    New Joke button added

--- server.py
+++ server.py
@@ -1,32 +1,28 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!', 200
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    first = request.form.get('first')
-    second = request.form.get('second')
-
-    response = jsonify( {'result' : int(first + second)} )
-    response = make_response(response)
-    response.status_code = 200
-    return response
-    
+    num = request.json
+    response = Result(num['first'] + num['second'])
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    first = request.form.get('first')
-    second = request.form.get('second')
-
-    response = jsonify( {'result' : int(first - second)} )
-    response = make_response(response)
-    response.status_code = 200
-    return response
+    num = request.json
+    response = Result(num['first'] - num['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
