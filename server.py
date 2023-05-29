173bc6434be59404ef9ada831b1931d1bed79a7b
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2023-05-29 15:10:25 +0530

    greeting, add, sub updated

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World', 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    first = request.form.get('first')
+    second = request.form.get('second')
+    return jsonify( {'result' : int(first) + int(second)} ), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    first = request.form.get('first')
+    second = request.form.get('second')
+    return jsonify( {'result' : int(first) - int(second)} ), 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
